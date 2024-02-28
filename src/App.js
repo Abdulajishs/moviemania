ed860752b7df8d1b20ffeaac0074be46c071d79e
Abdulajishs <abdulajeeshsb@gmail.com>
2024-02-28 11:44:13 +0530

    making api calls for fetching data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  // const fetchMoviesHandler = () => {
+  //   fetch("https://swapi.dev/api/films/")
+  //     .then((response) => {
+  //       return response.json()
+  //     })
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((moviedata) => {
+  //         return {
+  //           id: moviedata.episode_id,
+  //           title: moviedata.title,
+  //           openingText: moviedata.opening_crawl,
+  //           releaseDate: moviedata.release_date
+  //         }
+  //       })
+  //       setMovies(transformedMovies)
+  //     })
+  // }
+
+  const fetchMoviesHandler = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/films/")
+      const data = await response.json()
+
+      const transformedMovies = data.results.map((moviedata) => {
+        return {
+          id: moviedata.episode_id,
+          title: moviedata.title,
+          openingText: moviedata.opening_crawl,
+          releaseDate: moviedata.release_date
+        }
+      })
+      setMovies(transformedMovies)
+    } catch (error) {
+      console.error("Error fetching movies :" ,error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
